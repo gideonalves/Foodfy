d9df4a114dff09ad353b0f28646b4303facd378e
gideon <gideonam@yahoo.com.br>
2021-07-19 23:57:26 -0300

    correção de bug

--- src/app/controllers/AdminController.js
+++ src/app/controllers/AdminController.js
@@ -1,7 +1,7 @@
 const RecipesAdmin = require('../../models/RecipesAdmin')
-const { options } = require('../../routes')
 
 module.exports = {
+
     indexRecipe(req, res) {
         RecipesAdmin.all(function (recipes) {
             return res.render("admin/recipes/indexRecipe", { recipes })
@@ -13,9 +13,8 @@ module.exports = {
             return res.render("admin/recipes/createRecipe", { chefOptions: options })
         })
     },
-  
-    // Criar
-    async post(req, res) {
+
+    post(req, res) {
         const keys = Object.keys(req.body)
 
         for (key of keys) {
@@ -23,55 +22,87 @@ module.exports = {
                 return res.send('Preencha todos os campos!')
             }
         }
-        // Criar os Recipes
-        let results = await RecipesAdmin.create(req.body)
-        const recipe = results.rows[0]
-        // redireciona para a pagina show
-        return res.redirect(`/admin/recipes/${recipe.id}`)
-    },   
+        //        fucao    paramet,  paramet
+        RecipesAdmin.create(req.body, function (recipe) { // fuções callback
+            // console.log(recipe)
+            return res.redirect(`/admin/recipes/${recipe.id}`)
+        })
 
-    // Show
-    async showRecipe(req, res) {
-        const results = await RecipesAdmin.find(req.params.id)
-        const recipes = results.rows[0]
-
-        if (!recipes) return res.send("Recipes not found!")
-
-        return res.render("admin/recipes/showRecipe", { items: recipes })
     },
 
-    
-    async editRecipe(req, res) {
-        let results = await RecipesAdmin.find(req.params.id)
-        const recipes = results.rows[0]
-            if (!recipes) return res.send("Recipes não encontrado!")
+    showRecipe(req, res) {
+        RecipesAdmin.find(req.params.id, function (recipes) {
 
-        results = await RecipesAdmin.chefSelectOptions() 
-        const options = results.rows
+            if (!recipes) return res.send("Recipes not found!")
 
-        return res.render("admin/recipes/editRecipe", { items: recipes, chefOptions: options })          
+            return res.render("admin/recipes/showRecipe", { items: recipes })
+        })
     },
 
-    async put(req, res) {
+    editRecipe(req, res) {
+        RecipesAdmin.find(req.params.id, function (recipes) {
+            if (!recipes) return res.send("Recipes not found!")
+
+            RecipesAdmin.chefSelectOptions(function (options) {
+                return res.render("admin/recipes/editRecipe", { items: recipes, chefOptions: options })
+
+            })
+
+            // return res.render("admin/recipes/editRecipe", { items: recipes })
+        })
+    },
+
+    put(req, res) {
+        // essa parte verifica se o formulario ta vazio -------------------
         const keys = Object.keys(req.body)
+        // return res.send(keys)
 
         for (key of keys) {
             if (req.body[key] == "") {
                 return res.send('Please, fill all fields!')
             }
         }
-        console.log(req.body);
-        req.body.ingredients = req.body.ingredients.filter(function (item) {
-             return item != ""        
-        })         
+        // filtro do array ingredites para remover item vazio
+        req.body.ingredients = req.body.ingredients.filter(function (item) { // filter precisa retornar boolean se o boolean for verdadeiro ele mantem o item no array  se for false ele tira o item do array                     
+            // console.log(item != "")
+            return item != ""
 
-        await RecipesAdmin.updade(req.body)
-        return res.redirect(`/admin/recipes/${req.body.id}`)
+            // if(item == "") // aqui to falando que o item ta vazio
+            // {
+            //     return false // essa linha tira o item do arrey 
+            // }
+            // return true // essa linha mantem o item no arrey
+        })
+
+        // console.log(req.body.ingredients)
+        //-----------------------------------------------------------------
+        RecipesAdmin.updade(req.body, function () {
+            return res.redirect(`/admin/recipes/${req.body.id}`)
+        })
     },
 
-    async delete(req, res) {
-        await RecipesAdmin.delete(req.body.id)
-        return res.redirect(`/admin/recipes`)
+    delete(req, res) {
+        RecipesAdmin.delete(req.body.id, function () {
+            return res.redirect(`/admin/recipes`)
+        })
+
     },
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/models/RecipesAdmin.js
+++ src/models/RecipesAdmin.js
@@ -1,11 +1,11 @@
 const db = require('../config/db')
 const {date} =  require('../lib/utils')
 
-module.exports = { 
-    // indexRecipe
+module.exports = {
+
     all(callback) {
         db.query(`
-        SELECT recipes.id, recipes.title,chefs.name
+        SELECT recipes.id,recipes.title,chefs.name
         FROM recipes
         INNER JOIN chefs
         ON recipes.chef_id = chefs.id`,
@@ -16,8 +16,8 @@ module.exports = {
             })
     },
 
-    // Criar    req.body        
-    create(data) {
+    //        req.body        
+    create(data, callback) {
         //inserir dados no banco de dados
         const query = `
          INSERT INTO recipes (
@@ -41,26 +41,34 @@ module.exports = {
             date(Date.now()).iso
         ]
 
-        return db.query(query, values)
-    },
+        db.query(query, values, function (err, results) {
+            if (err) throw `Database Erro! ${err}`
 
+            callback(results.rows[0])
+        })
+    },
     // show
-    find(id) {
-        return db.query(`
+    find(id, callback) {
+        db.query(`
             SELECT * FROM recipes
-            WHERE id = $1`, [id])
+            WHERE id = $1`, [id], function (err, results) {
+            if (err) throw `Database Erro! ${err}`
+
+            callback(results.rows[0])
+        })
     },
 
-    updade(data) {
+    updade(data, callback) {
+
         const query = `
             UPDATE recipes SET
-                chef_id=($1),
-                image=($2),
-                title=($3),
-                ingredients=($4),
-                preparation=($5),
-                information=($6),
-                created_at=($7)
+            chef_id=($1),
+            image=($2),
+            title=($3),
+            ingredients=($4),
+            preparation=($5),
+            information=($6),
+            created_at=($7)
             WHERE id = $8
             `
         const values = [
@@ -73,18 +81,27 @@ module.exports = {
             data.created_at,
             data.id
         ]
-        return db.query(query, values)
+
+        db.query(query, values,
+            function (err, results) {
+                if (err) throw `Database Erro! ${err}`
+
+                callback()
+            }
+        )
     },
 
-    delete(id) {
-        return db.query(`
+    delete(id, callback) {
+        db.query(`
             DELETE FROM recipes 
-            WHERE id = $1`, [id])
+            WHERE id = $1`, [id],
+            function (err, results) {
+                if (err) throw `Database Erro! ${err}` // throw = lançar
+
+                return callback()
+            }
+        )
     },
-      // createRecipe e editRecipe
-    // chefSelectOptions() {        
-    //     return db.query(`SELECT name, id FROM chefs`)
-    // },
     chefSelectOptions(callback) {
         db.query(`
             SELECT * FROM chefs`,
@@ -101,10 +118,11 @@ module.exports = {
         return db.query(`
             SELECT * FROM recipes 
             WHERE chef_id = $1
-                `, [id]
+             `, [id]
         )
         // } catch(error) {
         //     throw error
         // }
     }
+
 }
